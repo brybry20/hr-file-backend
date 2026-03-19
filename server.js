a020b70bce43fa27d6aa3cb05de22abc95cdad18
brybry20 <bryan123james@gmail.com>
2026-03-19 09:24:50 +0800

    fixed

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ import { dirname, join } from 'path';
 import authRoutes from './routes/auth.js';
 import employeeRoutes from './routes/employees.js';
 import resignedRoutes from './routes/resigned.js';
-import bankAccountsRoutes from './routes/bankaccounts.js';
+import bankAccountsRoutes from './routes/bankAccounts.js';
 import hardwareRoutes from './routes/hardware.js';
 import phoneRoutes from './routes/phones.js';   
 import carsRoutes from './routes/cars.js';
